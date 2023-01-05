e1d58f8ff22e163857e67a5e19366c9b63724c7f
Diego Beker <diegobecker2007@hotmail.com>
2023-01-05 13:56:23 -0300

    Envio do pedido

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let contadorPrato = 0;
 let contadorBebida = 0;
 let contadorSobremesa = 0;
+let valorPrato = 0;
+let valorBebida = 0;
+let valorDoce = 0;
+let nomePrato,nomeBebida,nomeDoce,aux;
 
 function selecionarProduto(secao,item){
     const secaoSelecionada = document.querySelector(secao);
@@ -15,6 +19,8 @@ function selecionarProduto(secao,item){
     }
     
     const produtoSelecionado = secaoSelecionada.querySelector(item);
+    const nome = produtoSelecionado.querySelector('h4')
+    const valor = produtoSelecionado.querySelector('span');
     const icone = produtoSelecionado.querySelector('.icone');
     
     produtoSelecionado.classList.add('selecionado');
@@ -23,15 +29,24 @@ function selecionarProduto(secao,item){
     switch(secao){
         case '.pratos':
             contadorPrato = 1;
-            console.log(contadorPrato);
+            nomePrato = nome.textContent;
+            aux = valor.textContent.split(' ');
+            valorPrato = Number(aux[1].replace(',','.'));
+            console.log(valorPrato);
             break;
         case '.bebidas':
             contadorBebida = 1;
-            console.log(contadorBebida);
+            nomeBebida = nome.textContent;
+            aux = valor.textContent.split(' ');
+            valorBebida = Number(aux[1].replace(',','.'));
+            console.log(valorBebida);
             break;
         case '.doces':
             contadorSobremesa = 1;
-            console.log(contadorSobremesa);
+            nomeDoce = nome.textContent;
+            aux = valor.textContent.split(' ');
+            valorDoce = Number(aux[1].replace(',','.'));
+            console.log(valorDoce);
             break;
     }
     atualizarCarrinho();
@@ -42,7 +57,15 @@ function atualizarCarrinho(){
         const botao = document.querySelector('.botao');
         const texto = botao.querySelector('p');
         botao.classList.add('liberado');
-        botao.attributes.removeNamedItem('disabled');
+        if(botao.attributes.getNamedItem('disabled'))
+            botao.attributes.removeNamedItem('disabled');
         texto.innerHTML = "Fechar pedido";
     }
+}
+
+function fecharPedido(){
+    let total = valorPrato + valorBebida + valorDoce;
+    let pedido = `Olá, gostaria de fazer o pedido:\n- Prato: ${nomePrato}\n- Bebida: ${nomeBebida}\n- Sobremesa: ${nomeDoce}\nTotal: ${total.toFixed(2)}`;
+    let url = `https://wa.me/5519982200551?text=${encodeURIComponent(pedido)}`;
+    window.open(url);
 }
