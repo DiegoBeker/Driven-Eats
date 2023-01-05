81edf407de939bb779ef48cb972e3c8b5934f7e8
Diego Beker <diegobecker2007@hotmail.com>
2023-01-05 09:26:40 -0300

    Botao de finalizar pedido

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let contadorPrato = 0;
+let contadorBebida = 0;
+let contadorSobremesa = 0;
+
 function selecionarProduto(secao,item){
     const secaoSelecionada = document.querySelector(secao);
 
@@ -16,4 +20,29 @@ function selecionarProduto(secao,item){
     produtoSelecionado.classList.add('selecionado');
     icone.classList.add('check');
 
+    switch(secao){
+        case '.pratos':
+            contadorPrato = 1;
+            console.log(contadorPrato);
+            break;
+        case '.bebidas':
+            contadorBebida = 1;
+            console.log(contadorBebida);
+            break;
+        case '.doces':
+            contadorSobremesa = 1;
+            console.log(contadorSobremesa);
+            break;
+    }
+    atualizarCarrinho();
+}
+
+function atualizarCarrinho(){
+    if(contadorPrato===1 && contadorBebida===1 && contadorSobremesa===1){
+        const botao = document.querySelector('.botao');
+        const texto = botao.querySelector('p');
+        botao.classList.add('liberado');
+        botao.attributes.removeNamedItem('disabled');
+        texto.innerHTML = "Fechar pedido";
+    }
 }
