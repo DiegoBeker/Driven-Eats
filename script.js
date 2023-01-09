092f25e75b6c94726b9d0f8a9edb596b8bfd3ee8
Diego Beker <diegobecker2007@hotmail.com>
2023-01-08 23:41:05 -0300

    bux fix avaliador bonus

--- script.js
+++ script.js
@@ -45,6 +45,8 @@ function selecionarProduto(secao,item){
             aux = valor.textContent.split(' ');
             valorDoce = Number(aux[1].replace(',','.'));
             break;
+        default:
+            break;
     }
     atualizarCarrinho();
 }
@@ -54,36 +56,39 @@ function atualizarCarrinho(){
         const botao = document.querySelector('.botao');
         const texto = botao.querySelector('p');
         botao.classList.add('liberado');
-        if(botao.attributes.getNamedItem('disabled'))
+        if(botao.attributes.getNamedItem('disabled')){
             botao.attributes.removeNamedItem('disabled');
+        }
         texto.innerHTML = "Fechar pedido";
     }
 }
 
 function fecharPedido(){
+    const casaDecimal = 2; 
     const janela = document.querySelector('.janela');
     janela.classList.remove('hide');
     janela.querySelector('.ver-prato p').innerHTML = nomePrato;
-    janela.querySelector('.ver-prato span').innerHTML = valorPrato.toFixed(2).replace('.',',');
+    janela.querySelector('.ver-prato span').innerHTML = valorPrato.toFixed(casaDecimal).replace('.',',');
     janela.querySelector('.ver-bebida p').innerHTML = nomeBebida;
-    janela.querySelector('.ver-bebida span').innerHTML = valorBebida.toFixed(2).replace('.',',');
+    janela.querySelector('.ver-bebida span').innerHTML = valorBebida.toFixed(casaDecimal).replace('.',',');
     janela.querySelector('.ver-doce p').innerHTML = nomeDoce;
-    janela.querySelector('.ver-doce span').innerHTML = valorDoce.toFixed(2).replace('.',',');
+    janela.querySelector('.ver-doce span').innerHTML = valorDoce.toFixed(casaDecimal).replace('.',',');
     let total = valorPrato + valorBebida + valorDoce;
-    total = total.toFixed(2);
+    total = total.toFixed(casaDecimal);
     janela.querySelector('.total span').innerHTML = total.replace('.',',');
     const nome = prompt('Nome');
-    const endereco = prompt('Endereço')
-    pedido = `Olá, gostaria de fazer o pedido:\n- Prato: ${nomePrato}\n- Bebida: ${nomeBebida}\n- Sobremesa: ${nomeDoce}\nTotal: R$ ${total}\n\nNome: ${nome}\nEndereço: ${endereco}`;
+    const rua = prompt('Endereço')
+    pedido = `Olá, gostaria de fazer o pedido:\n- Prato: ${nomePrato}\n- Bebida: ${nomeBebida}\n- Sobremesa: ${nomeDoce}\nTotal: R$ ${total}\n\nNome: ${nome}\nEndereço: ${rua}`;
 }
 
 function enviarPedido(){
-    let url = `https://wa.me/5519999999999?text=${encodeURIComponent(pedido)}`;
+    const url = `https://wa.me/5519999999999?text=${encodeURIComponent(pedido)}`;
     window.open(url);
 }
 
 function cancelar(){
     const janela = document.querySelector('.janela');
-    if(janela!==null)
+    if(janela!==null){
         janela.classList.add('hide');
+    }
 }
